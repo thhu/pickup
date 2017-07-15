3316b8ff68e6e12ec387b354a15709ec9544eb6c
Tian Hu <tian.hu@uwaterloo.ca>
2017-07-14 22:13:30 -0400

    Added tokens to store profiles in firebase

--- pickup/RegsiterAccountViewController.swift
+++ pickup/RegsiterAccountViewController.swift
@@ -8,14 +8,19 @@
 
 import Foundation
 import FirebaseAuth
+import FirebaseDatabase
 
 class RegisterAccountViewController: UIViewController {
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
     
     @IBAction func registerAccount(_ sender: Any) {
-        Auth.auth().createUser(withEmail: email.text!, password: password.text!) { (user, error) in
+        Auth.auth().createUser(withEmail: self.email.text!, password: self.password.text!) { (user, error) in
             if (error == nil){
+                let ref = Database.database().reference().child("user").childByAutoId();
+                ref.setValue(["email": self.email.text])
+                UserDefaults.standard.setValue(ref.key, forKey: "token")
+                UserDefaults.standard.synchronize()
                 self.dismiss(animated: true, completion: nil)
             }
         }
