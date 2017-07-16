1034466166d04a0aab4300491527c4ada8eea79d
Tian Hu <tian.hu@uwaterloo.ca>
2017-07-16 01:52:33 -0400

    Plugged real data into friend list

--- pickup/FriendsListViewController.swift
+++ pickup/FriendsListViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import FirebaseDatabase
 
 class FriendsListViewCell: UITableViewCell {
     
@@ -14,19 +15,65 @@ class FriendsListViewCell: UITableViewCell {
     @IBOutlet weak var Name: UILabel!
 }
 
-class FriendsListViewController: UIViewController {
+class UserData {
+    var key: String!
+    var firstName: String!
+    var lastName: String!
+    var pictureUrl: String?
+}
+
+class FriendsListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     
     @IBOutlet weak var SearchBar: UISearchBar!
     @IBOutlet weak var FriendsTable: UITableView!
+    
+    var ref: DatabaseReference!
+    var userItems: [UserData] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.ref = Database.database().reference().child("user")
+        self.FriendsTable.delegate = self
+        self.FriendsTable.dataSource = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.ref.observe(DataEventType.value, with: { (snapshot) in
+            self.userItems.removeAll()
+            for item in snapshot.children {
+                let datasnapshot = item as! DataSnapshot
+                let dict = datasnapshot.value as! [String: Any]
+                    let data = UserData()
+                    data.firstName = dict["firstName"] as! String
+                    data.lastName = dict["lastName"] as! String
+                    data.pictureUrl = dict["pictureUrl"] as? String
+                    self.userItems.append(data)
+            }
+            self.FriendsTable.reloadData()
+        })
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.ref.removeAllObservers()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.userItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FriendCell", for: indexPath as IndexPath) as! FriendsListViewCell
+        cell.Name.text = userItems[indexPath.row].firstName + " " + userItems[indexPath.row].lastName
+        return cell
+    }
     
 }
